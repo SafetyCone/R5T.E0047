1574c53ad530bd4a82f88cd0f3eb8ce2d798f178
davidcoats <david@getsafetycone.com>
2023-02-13 23:58:05 -0800

    Interim changes.

--- source/R5T.E0047.F001/Code/Instances.cs
+++ source/R5T.E0047.F001/Code/Instances.cs
@@ -6,7 +6,7 @@ namespace R5T.E0047.F001
     public static class Instances
     {
         public static IFilePathProvider FilePathProvider { get; } = F001.FilePathProvider.Instance;
-        public static T0041.PathOperator PathOperator { get; } = T0041.PathOperator.Instance;
+        public static T0041.IPathOperator PathOperator { get; } = T0041.PathOperator.Instance;
         public static F0018.IReflectionOperator ReflectionOperations { get; } = F0018.ReflectionOperator.Instance;
     }
 }
